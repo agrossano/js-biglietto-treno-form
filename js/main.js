2631dd0b13ff6ac7f61cdce684b413a7c3cff9f9
Alessandro Grossano <grossano.alessandro@gmail.com>
2020-03-28 12:19:01 +0100

    CSS e logica JS complete

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 // dichiaro variabili
-var nameValue, kmValue, ageValue;
+var nameValue, kmValue, ageValue, carrozza, codice;
 
 // selezione campi input
 var nameG = document.getElementById('name');
 var kmG = document.getElementById('km');
 var ageG = document.getElementById('age');
 var ResetG = document.getElementById('reset');
-
+var ticketBox = document.getElementsByClassName('container-ticket');
 
 console.log(nameG, kmG, ageG)
 
@@ -34,12 +34,18 @@ generaBtn.addEventListener('click',
       ticketPrice = ticketPrice - (ticketPrice * discount / 100);
     }
 
+    //stampo nome e prezzo del biglietto solo se gli input sono riempiti e il nome è una stringa
 
-    //stampo nome del passeggero
-    document.getElementById('passenger-name').innerHTML = nameValue;
-    //stampo prezzo del biglietto
-    document.getElementById('ticket-price').innerHTML = '€ ' + ticketPrice.toFixed(2) + ' Tariffa ' + ageValue;
-  })
+    if (ticketPrice && nameValue) {
+      document.getElementById('passenger-name').innerHTML = nameValue;
+      document.getElementById('ticket-price').innerHTML = '€ ' + ticketPrice.toFixed(2) + ' Tariffa ' + ageValue;
+      ticketBox[0].className = ticketBox[0].classList + ' ticket-active ';
+    } else {
+      document.getElementById('passenger-name').innerHTML = '';
+      document.getElementById('ticket-price').innerHTML = '';
+    }
+  }
+)
 
 ResetG.addEventListener('click',
   function () {
@@ -53,4 +59,5 @@ ResetG.addEventListener('click',
     ageValue = '';
     ticketPrice = '';
     document.getElementById('ticket-price').innerHTML = '';
+    ticketBox[0].classList.remove("ticket-active");
   })
